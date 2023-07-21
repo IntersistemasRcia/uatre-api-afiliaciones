2332590d7bc3ed8401e9c77e585abba3ca7a9389
yosoyciro <ciro.daniele@gmail.com>
2023-07-21 18:31:51 -0300

    seccionales todos los campos

--- CleanArchitecture.Application/Features/Seccional/Queries/SeccionalVm.cs
+++ CleanArchitecture.Application/Features/Seccional/Queries/SeccionalVm.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string? Codigo { get; set; }
         public string? Descripcion { get; set; }
+        public string? Domicilio { get; set; }
+        public string? Estado { get; set; }
+        public string? Observaciones { get; set; }
     }
 }
