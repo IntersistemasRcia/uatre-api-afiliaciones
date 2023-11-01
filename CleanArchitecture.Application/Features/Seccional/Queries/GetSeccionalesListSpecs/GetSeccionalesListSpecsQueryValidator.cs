4339310a577b265bc43f0d6825f6de2821ccc395
yosoyciro <ciro.daniele@gmail.com>
2023-11-01 16:51:24 -0300

    ajustes

--- CleanArchitecture.Application/Features/Seccional/Queries/GetSeccionalesListSpecs/GetSeccionalesListSpecsQueryValidator.cs
+++ CleanArchitecture.Application/Features/Seccional/Queries/GetSeccionalesListSpecs/GetSeccionalesListSpecsQueryValidator.cs
@@ -2,12 +2,20 @@
 
 namespace CleanArchitecture.Application.Features.Seccional.Queries.GetSeccionalesListSpecs;
 
+public class AmbitoValidator : AbstractValidator<Ambito>
+{
+    public AmbitoValidator()
+    {
+        RuleFor(x => x.Tipo).NotEmpty().WithMessage("Debe especificar el Tipo");
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Debe especificar AmbitoId");
+    }
+    
+}
+
 public class GetSeccionalesListSpecsQueryValidator : AbstractValidator<GetSeccionalesListSpecsQuery>
 {
     public GetSeccionalesListSpecsQueryValidator()
     {
-        RuleFor(x => x.Ambitos).NotNull().NotEmpty().WithMessage("Debe especificar el Ambito");
-        RuleFor(x => x.Ambitos.Select(p => p.Tipo)).NotEmpty().WithMessage("Debe especificar el Tipo");
-        RuleFor(x => x.Ambitos.Select(p => p.Id)).NotEmpty().WithMessage("Debe especificar AmbitoId");
+        RuleForEach(x => x.Ambitos).SetValidator(new AmbitoValidator());        
     }
 }
