5dfada157799d4e36f414e70f1f10dca7ede5ea1
INtersistemas <binarianet.chaco@gmail.com>
2024-03-15 10:35:30 -0300

    ajuste

--- CleanArchitecture.Application/Models/APIComunes/DocumentacionEntidad.cs
+++ CleanArchitecture.Application/Models/APIComunes/DocumentacionEntidad.cs
@@ -3,13 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CleanArchitecture.Application.Models.APIComunes
 {
-    public class DocumentacionEntidad : EntidadAuditable
-    {        
+    public class DocumentacionEntidad
+    {
+        public int Id { get; set; }
         public string? EntidadTipo { get; set; }
         public int EntidadId { get; set; }
         public int RefTipoDocumentacionId { get; set; }
         public Byte[]? Archivo { get; set; }
         public string? NombreArchivo { get; set; }
-        public string? Observaciones { get; set; }       
+        public string? Observaciones { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public string? DeletedBy { get; set; }
+        public string? DeletedObs { get; set; }
     }
 }
