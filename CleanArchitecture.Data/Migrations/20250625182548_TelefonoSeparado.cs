0a8b1c494fd544101d7a46e08ec377a1b53470ae
Ciro Daniele <ciro.daniele@gmail.com>
2025-06-25 16:04:58 -0300

    cambios para manejar telefono por separado

---
+++ CleanArchitecture.Data/Migrations/20250625182548_TelefonoSeparado.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelefonoSeparado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TelefonoArea",
+                table: "Afiliados",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelefonoNumero",
+                table: "Afiliados",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelefonoPais",
+                table: "Afiliados",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TelefonoArea",
+                table: "Afiliados");
+
+            migrationBuilder.DropColumn(
+                name: "TelefonoNumero",
+                table: "Afiliados");
+
+            migrationBuilder.DropColumn(
+                name: "TelefonoPais",
+                table: "Afiliados");
+        }
+    }
+}
