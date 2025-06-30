0056cbd02a3487359c8505a152bbd4878f172a7f
Ciro Daniele <ciro.daniele@gmail.com>
2025-06-30 07:18:46 -0300

    ajuste filtro estado seccional segun ambito

--- CleanArchitecture.Application/Specification/Implements/AfiliadoSpecification.cs
+++ CleanArchitecture.Application/Specification/Implements/AfiliadoSpecification.cs
@@ -33,7 +33,7 @@ namespace CleanArchitecture.Application.Specification.Implements
             (!query.Documento.HasValue || x.Documento == query.Documento) &&
             (string.IsNullOrEmpty(query.Seccional) || x.Seccional!.Descripcion!.Contains(query.Seccional) || x.Seccional!.Codigo!.Contains(query.Seccional)) &&
             //(!query.SeccionalEstadoId.HasValue || x.Seccional!.SeccionalEstadoId == query.SeccionalEstadoId) && //se agrega a pedido de mauricio por el tema de los AMBITOS DELEGACIONES; Deben mostrar afiliados con seccionales Normalizadas. Hoy 09/06/25 se quita este filtro que era exclusivo de ambito Delegaciones
-            (query.AmbitoTodos == null && x.Seccional!.SeccionalEstado!.Descripcion.Contains("NORMALIZADA") || x.Seccional!.SeccionalEstado!.Descripcion.Contains("TRANSITORIA") || x.Seccional!.SeccionalEstado!.Descripcion.Contains("SIN COMISION")) &&
+            ((query.AmbitoTodos == null || query.AmbitoTodos.Ids.FirstOrDefault() == 0) || (x.Seccional!.SeccionalEstado!.Descripcion.Contains("NORMALIZADA") || x.Seccional!.SeccionalEstado!.Descripcion.Contains("TRANSITORIA") || x.Seccional!.SeccionalEstado!.Descripcion.Contains("SIN COMISION"))) &&
             (!query.SoloActivos || x.DeletedDate == null) &&
             (!query.FechaIngreso.HasValue || (x.FechaIngreso.Value.Date >= query.FechaIngreso.Value.Date && x.FechaIngreso.Value.Date <= query.FechaIngresoHasta.Value.Date)) &&
             (!query.FechaEgreso.HasValue || x.FechaEgreso.Value.Date == query.FechaEgreso.Value.Date) &&
