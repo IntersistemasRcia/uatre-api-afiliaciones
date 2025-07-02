c0897cec4a79081c11b47f560c55a5c12118ad04
Ciro Daniele <ciro.daniele@gmail.com>
2025-07-02 18:56:49 -0300

    ajustes ambitos

---
+++ CleanArchitecture.Application/Specification/Implements/SeccionalConEstadoSpec.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Application.Features.Seccional.Queries.GetSeccionalesListSpecs;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Specification.Implements;
+
+public class SeccionalConEstadoSpec : BaseSpecification<Domain.Seccional>
+{
+    public SeccionalConEstadoSpec(Ambito ambito, List<string> estadosActiva) : base(x => ambito.Ids.Contains(x.Id)
+    && estadosActiva.Contains(x.SeccionalEstado.Descripcion))
+    {
+        AgregarIncludes(x => x.Include(e => e.SeccionalEstado!));        
+    }
+}
