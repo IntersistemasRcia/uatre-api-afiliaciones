e17758b02b5afa2f30e0e2d0f16d2a01cc1ff2f1
Ciro Daniele <ciro.daniele@gmail.com>
2026-06-05 18:07:08 -0300

    Feat/seccionales ignorarestado (#40)
    * ajustes filtro de seccional en consulta de afiliados
    
    * ajuste orden en afiliados

--- application/Contracts/Specification/ISpecification.cs
+++ application/Contracts/Specification/ISpecification.cs
@@ -2,37 +2,40 @@
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
-namespace CleanArchitecture.Application.Contracts.Specification
+namespace CleanArchitecture.Application.Contracts.Specification;
+
+public interface ISpecification<T>
 {
-    public interface ISpecification<T>
+    //Filtro
+    Expression<Func<T, bool>> Criteria { get; }
+
+    //Include de tablas relacionadas
+    List<Func<IQueryable<T>, IIncludableQueryable<T, object>>> Includes { get; }
+    //List<string> IncludeStrings { get; }
+
+    //Ordenamiento
+    List<OrderDetails> Order { get; set; }
+
+    ISet<OrderDetails> OrderSet { get; set; }
+
+    List<Expression<Func<T, object>>> OrderBy { get; }
+
+    List<Expression<Func<T, object>>> OrderByDesc { get; }        
+
+    //Agrupar
+    Expression<Func<T, object>> GroupBy { get; }
+
+    //Take
+    int? TakeRecords { get; }
+
+    //Paginacion
+    int Take { get; }
+    int Skip { get; }
+    bool IsPagingEnabled { get; }
+
+    public class OrderDetails
     {
-        //Filtro
-        Expression<Func<T, bool>> Criteria { get; }
-
-        //Include de tablas relacionadas
-        List<Func<IQueryable<T>, IIncludableQueryable<T, object>>> Includes { get; }
-        //List<string> IncludeStrings { get; }
-
-        //Ordenamiento
-        List<OrderDetails> Order { get; set; }
-        List<Expression<Func<T, object>>> OrderBy { get; }
-        List<Expression<Func<T, object>>> OrderByDesc { get; }        
-
-        //Agrupar
-        Expression<Func<T, object>> GroupBy { get; }
-
-        //Take
-        int? TakeRecords { get; }
-
-        //Paginacion
-        int Take { get; }
-        int Skip { get; }
-        bool IsPagingEnabled { get; }
-
-        public class OrderDetails
-        {
-            public Expression<Func<T, object>> Order { get; set; }
-            public bool descending { get; set; }
-        }
+        public Expression<Func<T, object>> Order { get; set; }
+        public bool Descending { get; set; }
     }
 }
