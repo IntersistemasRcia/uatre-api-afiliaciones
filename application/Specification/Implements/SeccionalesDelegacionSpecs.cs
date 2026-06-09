91234f91080958c8e605cee276fa503f11e99b5c
Ciro Daniele <ciro.daniele@gmail.com>
2026-06-09 16:12:09 -0300

    ajustes para mostrar afiliados de seccionales ignorando el estado

--- application/Specification/Implements/SeccionalesDelegacionSpecs.cs
+++ application/Specification/Implements/SeccionalesDelegacionSpecs.cs
@@ -6,7 +6,7 @@ namespace CleanArchitecture.Application.Specification.Implements;
 public class SeccionalesDelegacionSpec : BaseSpecification<Domain.Seccional>
 {
     public SeccionalesDelegacionSpec(Ambito ambito, List<string> estadosActiva) : base(x => ambito.Ids.Contains(x.RefDelegacionId)
-    && estadosActiva.Contains(x.SeccionalEstado.Descripcion))
+    && (estadosActiva.FirstOrDefault() == "TODOS" || estadosActiva.Contains(x.SeccionalEstado.Descripcion)))
     {
         AgregarIncludes(x => x.Include(e => e.SeccionalEstado!));
     }
